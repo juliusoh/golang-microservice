ec8c7893ede3a3330e79eadd6871b1f07d76fc46
Julius Oh <juliusoh@gmail.com>
2022-09-07 05:12:02 -0700

    unit test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,8 +14,11 @@ const (
 	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
 )
 
+// Global variable, use it in other test files
+// DB Connection to create Queries Object
 var testQueries *Queries
 
+// Main entry point of all unit tests inside this package db
 func TestMain(m *testing.M) {
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
